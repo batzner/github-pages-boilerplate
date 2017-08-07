b0437d1034ecce3d8db5e7a6e97943aeda222646
batzner <github@kilians.net>
2017-08-08 00:06:33 +0200

    Deploying build

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+[1, 2, 3].forEach(function (x) {
+    return showItWorks();
+});
+
+function showItWorks() {
+    document.getElementById("javascript-status").innerHTML = "JavaScript transpilation works!";
+}
