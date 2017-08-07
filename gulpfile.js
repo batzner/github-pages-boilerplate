6d60e850a4d85d7d55d05dba6139ee1e23e9b2d9
batzner <github@kilians.net>
2017-08-07 23:53:39 +0200

    Added all necessary files

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+/**
+ * Basic concept follows https://www.justinmccandless.com/post/a-tutorial-for-getting-started-with-gulp/
+ */
+const gulp = require('gulp');
+
+// Include plug-ins
+const clean = require('gulp-clean');
+const babel = require('gulp-babel');
+
+// Base for specifying paths in gulp.src and gulp.dest
+const distDir = '_dist/';
+
+// Clean the dist directory
+gulp.task('_clean', function () {
+    return gulp.src(distDir)
+        .pipe(clean());
+});
+
+// Copy all files to the dist directory
+gulp.task('_copy', ['_clean'], function () {
+    return gulp.src(['src/**/*']).pipe(gulp.dest(distDir));
+});
+
+// Transpile the javascript files to ES5 in the dist directory (in-place)
+gulp.task('_javascripts', ['_copy'], function () {
+    return gulp.src('js/**/*.js', {base: distDir, cwd: distDir})
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest(distDir));
+});
+
+// Default task producing a jekyll-ready site in the dist folder
+gulp.task('default', ['_javascripts']);
+
+// Run Jekyll
+gulp.task('jekyll', ['default'], function (gulpCallBack) {
+    let spawn = require('child_process').spawn;
+    let jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+
+    jekyll.on('exit', function (code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+    });
+});
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+[1,2,3].forEach(showItWorks);
+
+
+function showItWorks() {
+    document.getElementById("javascript-status").innerHTML = "JavaScript transpilation works!"
+}
