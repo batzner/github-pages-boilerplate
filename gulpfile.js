6aa5b786af508414e5867f39999b79e8ac3559ce
Colton Loewen <loewencjay@outlook.com>
2019-03-11 02:37:40 -0600

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,28 +13,29 @@ const distDir = './';
 
 // Copy all files to the dist directory. On the master-branch, we cannot clean the dist directory
 // first if it is the root directory.
-gulp.task('_copy', function () {
+function _copy() {
     return gulp.src(['src/**/*']).pipe(gulp.dest(distDir));
-});
+};
 
 // Transpile the javascript files to ES5 in the dist directory (in-place)
-gulp.task('_javascripts', ['_copy'], function () {
+// Transpile the javascript files in the dist directory (in-place)
+function _javascripts() {
     return gulp.src('js/**/*.js', {base: distDir, cwd: distDir})
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(gulp.dest(distDir));
-});
+};
 
 // Default task producing a jekyll-ready site in the dist folder
-gulp.task('default', ['_javascripts']);
+gulp.task('default', gulp.series(_copy, _javascripts));
 
 // Run Jekyll
-gulp.task('jekyll', ['default'], function (gulpCallBack) {
+gulp.task('jekyll', gulp.series('default', function (gulpCallBack) {
     let spawn = require('child_process').spawn;
     let jekyll = spawn('jekyll', ['serve'], {stdio: 'inherit'});
 
     jekyll.on('exit', function (code) {
         gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
     });
-});
+}));
