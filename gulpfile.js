8895e4e09dbd7301efe7738eea76438e182b7d75
Colton Loewen <loewencjay@outlook.com>
2019-03-11 03:31:04 -0600

    Added changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,41 +6,40 @@
 const gulp = require('gulp');
 
 // Include plug-ins
-const clean = require('gulp-clean');
 const babel = require('gulp-babel');
+const del = require('del');
 
 // Base for specifying paths in gulp.src and gulp.dest
 const distDir = '_dist/';
 
 // Clean the dist directory
-gulp.task('_clean', function () {
-    return gulp.src(distDir)
-        .pipe(clean());
-});
+function _clean() {
+    return del(distDir);
+};
 
 // Copy all files to the dist directory
-gulp.task('_copy', ['_clean'], function () {
+function _copy() {
     return gulp.src(['src/**/*']).pipe(gulp.dest(distDir));
-});
+};
 
 // Transpile the javascript files to ES5 in the dist directory (in-place)
-gulp.task('_javascripts', ['_copy'], function () {
+function _javascripts() {
     return gulp.src('js/**/*.js', {base: distDir, cwd: distDir})
         .pipe(babel({
-            presets: ['es2015']
+            presets: ['@babel/preset-env']
         }))
         .pipe(gulp.dest(distDir));
-});
+};
 
 // Default task producing a jekyll-ready site in the dist folder
-gulp.task('default', ['_javascripts']);
+gulp.task('default', gulp.series(_clean, _copy, _javascripts));
 
 // Run Jekyll
-gulp.task('jekyll', ['default'], function (gulpCallBack) {
+gulp.task('jekyll', gulp.series('default', function (gulpCallBack) {
     let spawn = require('child_process').spawn;
     let jekyll = spawn('jekyll', ['serve'], {stdio: 'inherit'});
 
     jekyll.on('exit', function (code) {
         gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
     });
-});
+}));
